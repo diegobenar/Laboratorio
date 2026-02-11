a06466e9bd45088d164b1d05cf4ade90f36814cc
diegobenar <diego.benavente11@icloud.com>
2026-02-10 23:18:28 -0600

    Laboratorio 3

---
+++ Lab03-A2-DiegoBenavente/Lab03-A2-DiegoBenavente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Laboratorio03
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            string curso;
+            Console.Write("Ingrese su curso: ");
+            curso = Console.ReadLine();
+            Console.WriteLine("Hola "+ nombre + ", seguro vas a pasar el curso "+  curso+ ", nunca te rindas.");
+            Console.WriteLine("Bye bye medicina.");
+            Console.Write("Presiona cualquier tecla para continuar...");
+            Console.ReadKey();
+            
+        }
+
+
+
+    }
+
+
+}
