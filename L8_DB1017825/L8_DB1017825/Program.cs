c9cbf51f85b0e7a4a8e9539ee4311818b5d9e269
diegobenar <diego.benavente11@icloud.com>
2026-03-11 09:40:33 -0600

    Laboratorio 8 - primera entrega

---
+++ L8_DB1017825/L8_DB1017825/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int contador = 1;
+        int aprobado = 0;
+        int desaprobado = 0;
+        double sumaNotas = 0;
+        double notaActual;
+
+        while (contador <= 10)
+        {
+            Console.Write("Ingrese la nota del estudiante " + contador + ": ");
+            notaActual = double.Parse(Console.ReadLine());
+          
+
+            if (notaActual < 0 || notaActual > 100)
+            {
+                Console.WriteLine("Nota inválida. Ingrese un valor entre 0 y 100.");
+                continue;
+            }
+
+            if (notaActual >= 61)
+            {
+                aprobado++;
+            }
+
+            else
+            {
+                desaprobado++;
+            }
+            sumaNotas += notaActual;
+            contador++;
+        }
+
+        double promedioClase = sumaNotas / 10;
+        Console.WriteLine("Cantidad de aprobados: " + aprobado);
+        Console.WriteLine("Cantidad de desaprobados: " + desaprobado);
+        Console.WriteLine("Promedio de la clase: " + promedioClase);
+    }
+}
